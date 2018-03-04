62bf012645fe0ffcf5543e3cc87754631d55ce3a
Jens Egholm <jensegholm@protonmail.com>
2018-03-04 16:05:04 +0100

    Generated blind mazes

--- src/Data/Maze.hs
+++ src/Data/Maze.hs
@@ -1,18 +1,18 @@
 -- \ A T-Maze generator and executor for use in online learning systems
-module Data.Maze (generateMaze, Direction, MazeNode(Exit, Entry)) where
+module Data.Maze (generateMaze, Direction(Left, Right, Back), MazeNode(Exit, Entry)) where
+
+import System.Random
 
 data Direction = Left | Right | Back deriving Show
 
 data MazeNode a = Blind | Entry | Exit
-                | Fork { left :: a, right :: a, parent :: MazeNode a }
-                deriving (Eq)
-
-instance (Show a) => Show (MazeNode a) where
-  show Blind = "Blind"
-  show Entry = "Entry"
-  show Exit = "Exit"
-  show (Fork left right _) = "Fork " ++ (show left) ++ " " ++ (show right)
+                | Fork { left :: MazeNode a, right :: MazeNode a }
+                deriving (Eq, Show)
 
 generateMaze :: Integer -> MazeNode Integer
 generateMaze 0 = Exit
-generateMaze n = Exit
+generateMaze n = generateBlindMaze n
+
+generateBlindMaze :: Integer -> MazeNode Integer
+generateBlindMaze 0 = Blind
+generateBlindMaze n = Fork (generateBlindMaze (n - 1)) (generateBlindMaze (n - 1))
