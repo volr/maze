3fff909523a4c42f527bf1eaa7dd7301cf5252c0
Jens Egholm <jensegholm@protonmail.com>
2018-03-05 14:33:27 +0100

    Ran it through a linter..

--- src/Data/Maze.hs
+++ src/Data/Maze.hs
@@ -1,21 +1,35 @@
--- \ A T-Maze generator and executor for use in online learning systems
+-- \ A T-Maze generator and evaluator for use in machine learning systems
 module Data.Maze (
   contains, generateMaze,
   Direction(Left, Right, Back),
   MazeNode(Blind, Exit, Entry, Fork)
 ) where
 
+
+import Data.List
 import System.Random
 
+-- | The only three directions an actor can move
 data Direction = Left | Right | Back deriving Show
 
-data MazeNode a = Blind | Entry | Exit
+-- | Features in the maze which will appear at each fork (choice)
+--type Features a = List a
+
+-- | The four possible types of nodes featured in a T-maze
+data MazeNode a = Blind -- ^ A blind path
+                | Entry  -- ^ The maze entry
+                | Exit  -- ^ The maze exit
+                -- | A fork which gives the actor a choice: left or right
                 | Fork { left :: MazeNode a, right :: MazeNode a }
                 deriving (Eq, Show)
 
+-- | Strategies for
+newtype FeatureStrategy = Consistent Integer
+
+-- | Tests whether a maze contains a given node
 contains :: MazeNode a -> MazeNode a -> Bool
 contains (Fork mazeLeft mazeRight) node =
-  (contains mazeLeft node) || (contains mazeRight node)
+  contains mazeLeft node || contains mazeRight node
 contains a b = a == b
 
 generateMaze :: (RandomGen g) => g -> Integer -> MazeNode Integer
@@ -24,9 +38,9 @@ generateMaze g n =
   let (isLeftExit, genBranch) = random g
       branchBlind = generateBlindMaze (n - 1)
       branchExit = generateMaze genBranch (n - 1)
-  in case isLeftExit of
-    True -> Fork branchBlind branchExit
-    False -> Fork branchExit branchBlind
+  in if isLeftExit
+    then Fork branchExit branchBlind
+    else Fork branchBlind branchExit
 
 generateBlindMaze :: Integer -> MazeNode Integer
 generateBlindMaze 0 = Blind
--- test/Data/MazeSpec.hs
+++ test/Data/MazeSpec.hs
@@ -6,7 +6,6 @@ import qualified Test.QuickCheck
 import System.Random (mkStdGen)
 
 import Data.Maze
-import Data.Maze (MazeNode(Blind, Exit, Entry, Fork))
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -18,14 +17,15 @@ spec = do
   let gen = mkStdGen 0
 
   describe "a maze" $ do
-    it "can generate an empty maze" $ do
-      generateMaze gen 0 `shouldBe` Exit
 
     it "can test if it contains a node" $ do
       contains Exit Exit `shouldBe` True
       contains Exit Entry `shouldBe` False
       contains (Fork Exit Blind) Blind `shouldBe` True
       contains (Fork Exit Exit) Blind `shouldBe` False
+
+    it "can generate an empty maze" $ 
+      generateMaze gen 0 `shouldBe` Exit
 
     it "can generate a maze with one level" $ do
       let maze = generateMaze gen 1
